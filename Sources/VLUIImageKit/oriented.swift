8b7f9c4813ef349b9b1c18530a916709602d5f17
VLstack <github@vlstack.fr>
2025-01-26 09:56:13 +0100

    add orientedUp property

---
+++ Sources/VLUIImageKit/oriented.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+extension UIImage
+{
+ /**
+  A UIImage instance with corrected orientation.
+  If the instance's orientation is already `.up`, it simply returns the original.
+  - Returns: An optional UIImage that represents the correctly oriented image.
+ */
+ var orientedUp: UIImage?
+ {
+  if imageOrientation == .up { return self }
+
+  UIGraphicsBeginImageContextWithOptions(size, false, scale)
+  draw(in: CGRect(origin: .zero, size: size))
+  let normalizedImage = UIGraphicsGetImageFromCurrentImageContext()
+  UIGraphicsEndImageContext()
+
+  return normalizedImage
+ }
+}
+
